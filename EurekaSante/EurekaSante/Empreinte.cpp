cb1452f6e1a64c43eb5e51c28f4e26fc53bd8b7e
tempteamcode <38981204+tempteamcode@users.noreply.github.com>
2018-05-04 16:15:02 +0200

    code

--- EurekaSante/EurekaSante/Empreinte.cpp
+++ EurekaSante/EurekaSante/Empreinte.cpp
@@ -20,14 +20,14 @@ Empreinte::~Empreinte()
 {
 }
 
-void Empreinte::Afficher() {
-	vector<attributString>::iterator itS;
-	vector<attributDouble>::iterator itD;
-	for (itS = attributsString.begin(); itS != attributsString.end(); ++itS) {
-		cout << itS->nom << " : "<< itS->valeur << ",";
+void Empreinte::Afficher() const {
+	vector<string>::const_iterator itS;
+	vector<double>::const_iterator itD;
+	for (itS = attributsString.cbegin(); itS != attributsString.cend(); ++itS) {
+		cout << itS << ";";
 	}
-	for (itD = attributsDouble.begin(); itD != attributsDouble.end(); ++itD) {
-		cout << itD->nom << " : " << itD->valeur << ",";
+	for (itD = attributsDouble.cbegin(); itD != attributsDouble.cend(); ++itD) {
+		cout << itD << ";";
 	}
 	cout << endl;
 }
