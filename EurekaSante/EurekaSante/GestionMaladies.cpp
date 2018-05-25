e6707ffaa10df2e71a006d6d8dfa4ac21d543bcb
doomoftheworld <zhenyu.yang@insa-lyon.fr>
2018-05-25 17:13:31 +0200

    pp

--- EurekaSante/EurekaSante/GestionMaladies.cpp
+++ EurekaSante/EurekaSante/GestionMaladies.cpp
@@ -1,30 +1,39 @@
 #include "GestionMaladies.h"
 
-/*void AfficherUneMaladie(Maladie m,Attributs a) {
-	int i = 0;
-	vector<double>::const_iterator itD;
-	cout << "Le nom de la maladie est : " << m.nom << endl;
-	cout << "Les moyennes de chaque attribut :";
-	for (itD = m.moyennes.cbegin(); itD != m.moyennes.cend(); ++itD) {
-		cout << *itD << " ";
-	}
-	cout << endl;
-	cout << "Les écarts types de chaque attribut :";
-	for (itD = m.ecartTypesDouble.cbegin(); itD != m.ecartTypesDouble.cend(); ++itD) {
-		cout << *itD << " ";
-	}
-}*/
+
 
 void AfficherUneMaladie(const Maladie& m, const Attributs& a) {
 	vector<double>::const_iterator itD;
+	vector<freqAttr>::const_iterator itSI;
 	cout << "Le nom de la maladie est : " << m.nom << endl;
-	cout << "Les moyennes de chaque attribut :";
+	cout << "Les moyennes de chaque attribut double :";
+	int indice = 0;
 	for (itD = m.moyennes.cbegin(); itD != m.moyennes.cend(); ++itD) {
+		while (a.IsDouble(indice) == false && indice < a.Compte()) {
+			indice++;
+		}
+		cout << a.GetName(indice) << " :";
 		cout << *itD << " ";
+		indice++;
 	}
 	cout << endl;
-	cout << "Les écarts types de chaque attribut :";
+	indice = 0;
+	cout << "Les écarts types de chaque attribut double :";
 	for (itD = m.ecartTypesDouble.cbegin(); itD != m.ecartTypesDouble.cend(); ++itD) {
+		while (a.IsDouble(indice) == false && indice < a.Compte()) {
+			indice++;
+		}
+		cout << a.GetName(indice) << " :";
 		cout << *itD << " ";
+		indice++;
+	}
+	cout << endl;
+	for (itSI = m.frequenceAttributString.cbegin(); itSI != m.frequenceAttributString.cend(); ++itD) {
+		while (a.IsDouble(indice) == false && indice < a.Compte()) {
+			indice++;
+		}
+		cout << a.GetName(indice) << " :";
+		cout << *itD << " ";
+		indice++;
 	}
 }
