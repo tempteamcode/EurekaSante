5dc8dc857a9cabcd020237e4a7f56531130b10b8
doomoftheworld <zhenyu.yang@insa-lyon.fr>
2018-06-01 16:34:37 +0200

    corrige

--- EurekaSante/EurekaSante/LigneHistorique.cpp
+++ EurekaSante/EurekaSante/LigneHistorique.cpp
@@ -2,5 +2,7 @@
 
 vector<string>* LigneHistorique::toString() {
 	vector<string>* contenu = new vector<string>;
+	contenu->push_back("L'utilisteur " + monutilisateur->GetNom() + " a fait les analyses ci-dessous à " + date + " :");
 
+	return contenu;
 }
--- EurekaSante/EurekaSante/LigneHistorique.h
+++ EurekaSante/EurekaSante/LigneHistorique.h
@@ -1,22 +1,28 @@
 #ifndef LIGNEHISTORIQUE_H
 #define LIGNEHISTORIQUE_H
 #include <time.h>
+#include <ctime>
 #include "Utilisateur.h"
 #include "Analyse.h" 
-typedef tm* dateJ;
 class LigneHistorique
 {
 
 protected:
-	dateJ date;
-	Utilisateur& monutilisateur;
-	Analyse& a;
+	string date;
+	Utilisateur* monutilisateur;
+	//Analyse& a;
 public:
-	LigneHistorique(const Utilisateur& monutilisateur,const Analyse& analyse) {
+	LigneHistorique(Utilisateur* utilisateur) {
 		time_t t;
-		date = localtime(&t);
-		this->monutilisateur = monutilisateur;
-		a = analyse;
+		time(&t);
+		tm date;
+		localtime_s(&date, &t);
+		char yyyymmdd[16];
+		std::strftime(yyyymmdd, sizeof(yyyymmdd), "%Y-%m-%d", &date);
+		this->date = yyyymmdd;
+		//date = dateJ;
+		monutilisateur = utilisateur;
+		//a = analyse;
 	}
 	~LigneHistorique() = default;
 	vector<string>* toString();
