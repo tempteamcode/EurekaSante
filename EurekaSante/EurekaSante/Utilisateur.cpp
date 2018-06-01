9069fb93c6424327e7a79e62771da11de6dc3536
doomoftheworld <zhenyu.yang@insa-lyon.fr>
2018-06-01 15:56:48 +0200

    rien

---
+++ EurekaSante/EurekaSante/Utilisateur.cpp
@@ -0,0 +1,24 @@
+#include "Utilisateur.h"
+
+Utilisateur::Utilisateur(const string& nom, const string& mdp) {
+	this->nom = nom;
+	this->mdp = mdp;
+}
+
+void Utilisateur::LancerAnalyse(string nomfichier) {
+	Attributs att;
+	vector<Empreinte*> empreintes;
+	vector<Maladie*> maladies;
+	vector<double> maladiesAssociees;
+	if (!FichierChargerAttributs("HealthMeasurementDescription.txt", att)) {
+		cerr << "Erreur lors du chargement des attributs !" << endl;
+	}
+	else {
+		if (!FichierChargerEmpreintes(nomfichier, att, empreintes, maladies)) {
+			cerr << "Erreur lors du chargement des empreintes+maladies !" << endl;
+		}
+		else {
+		}
+	}
+}
+
---
+++ EurekaSante/EurekaSante/Utilisateur.h
@@ -0,0 +1,28 @@
+#ifndef UTILISATEUR_H
+#define UTILISATEUR_H
+
+#include "Reglages.h"
+#include "Empreinte.h"
+#include "Maladie.h"
+#include "GestionFichiers.h"
+#include <iostream>
+#include "Analyse.h"
+using namespace std;
+
+class Utilisateur
+{
+
+protected:
+	string nom;
+	string mdp;
+
+public:
+	Utilisateur(const string& nom, const string& mdp);
+	~Utilisateur() = default;
+	string GetNom() {
+		return nom;
+	}
+	void LancerAnalyse(string nomfichier);
+};
+
+#endif // UTILISATEUR_H
